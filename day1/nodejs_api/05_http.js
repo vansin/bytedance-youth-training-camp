9562c4110beada576b99098a567fd6989ffd8212
vansin <vansin@aliyun.com>
2021-09-20 10:53:20 +0800

    feat: winter  yyds from index.html

--- day1/nodejs_api/05_http.js
+++ day1/nodejs_api/05_http.js
@@ -1,40 +1,8 @@
-const fs = require('fs')
-const http = require('http')
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-http.createServer((request, response)=>{
-    // console.log('request', getPrototypeChain(request))
-
-    const {url, method} = request
-    // if (url === '/' && method === 'GET') {
-    //     fs.readFile('index.html',(err, data) => {
-    //         if (err) {
-    //         response.writeHead(500, {
-    //             'Content-Type': 'text/plain;charset=utf-8'
-    //         })
-    //         response.end('500 服务器挂了')
-    //         return
-    //     }
-
-    //     response.statusCode = 200
-    //     // response.setHeader({
-    //     //     'Content-Type':'text/html'
-    //     // })
-
-    //     response.end(data)
-    //     })
-    //     response.end('winter yyds')
-    // }
-    response.end('winter yyds')
-
-})
-.listen(3000, ()=> {
-    console.log('server at 3000')
-})
-
-function  getPrototypeChain(obj) {
-    const protoChain = []
-    while (obj = Object.getPrototypeOf(obj)) {
-        protoChain.push(obj)
-    }
-    return protoChain 
-}
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'html'});
+  res.end(index);
+}).listen(3000);
