8f82c73284a1e84089b44eef5b827272e7399f62
vansin <vansin@aliyun.com>
2021-09-20 14:36:39 +0800

    feat: auto npm run serve

--- day1/02_vue-auto-router-cli/lib/init.js
+++ day1/02_vue-auto-router-cli/lib/init.js
@@ -5,10 +5,44 @@ const clear = require('clear')
 const chalk = require('chalk')
 const log = content => console.log(chalk.green(content))
 const {clone} = require('./download')
+// const { spawn } = require('child_process')
+const { resolve } = require('path')
+
+
+// require('child_process')
+const spawn = async (...args) => {
+    const { spawn } = require('child_process')
+    return new Promise(resolve => {
+        const proc = spawn(...args)
+        proc.stdout.pipe(process.stdout)
+        proc.stderr.pipe(process.stderr)
+        proc.on('close', () => {
+            resolve()
+        })
+    })
+}
+
 module.exports = async name => {
-    // 打印欢迎界面
+    // 打印欢迎画面
     clear()
-    const data = await figlet('vansin welcome')
+    const data = await figlet('KKB Welcome')
     log(data)
-    await clone('github:vansin/serverless_proxy#main',name)
+    // 创建项目
+    log(`创建项目:` + name)
+    // 克隆代码
+    await clone('github:su37josephxia/vue-template', name)
+    log('安装依赖')
+    await spawn('npm', ['install'], { cwd: `./${name}` })
+    log(`
+👌安装完成：
+To get Start:
+===========================
+    cd ${name}
+    npm run serve
+===========================
+            `)
+
+    const open = require('open')
+    open('http://localhost:8080')
+    await spawn('npm', ['run', 'serve'], { cwd: `./${name}` })
 }
