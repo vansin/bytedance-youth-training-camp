cc52218271c1092c00a2a9aaa3d7276939e33e18
vansin <vansin@aliyun.com>
2021-09-20 13:14:01 +0800

    fix: delete the unuse code

--- day1/02_vue-auto-router-cli/lib/init.js
+++ day1/02_vue-auto-router-cli/lib/init.js
@@ -4,10 +4,11 @@ const figlet = promisify(require('figlet'))
 const clear = require('clear')
 const chalk = require('chalk')
 const log = content => console.log(chalk.green(content))
-
+const {clone} = require('./download')
 module.exports = async name => {
     // 打印欢迎界面
     clear()
     const data = await figlet('vansin welcome')
     log(data)
+    await clone('github:vansin/serverless_proxy#main',name)
 }
