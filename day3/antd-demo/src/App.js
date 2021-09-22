3733cdba5b779b592e01f98b4f71f7589c2c5916
vansin <vansin@aliyun.com>
2021-09-22 12:21:15 +0800

    feat: add button

--- day3/antd-demo/src/App.js
+++ day3/antd-demo/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import { DatePicker, Menu, Button } from 'antd';
+import antd_svg from './antd.svg';
+import { DatePicker, Menu, Button, Row, Col, Input } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import {
@@ -18,9 +19,14 @@ const { SubMenu } = Menu;
 function App() {
   return (
     <div className="App">
+       <Row>
+        <Col>
         <h1><img alt="logo" src={logo}></img>Ant Design</h1>
-        {/* <DatePicker /> */}
-
+        </Col>
+        <Col>
+        <Input placeholder="Basic usage" />
+        </Col>
+        <Col>
         <Menu mode="horizontal">
           <Menu.Item key="1" icon={<PieChartOutlined />}>
             设计
@@ -31,23 +37,30 @@ function App() {
           <Menu.Item key="3" icon={<ContainerOutlined />}>
             组件
           </Menu.Item>
-          <SubMenu key="sub1" icon={<MailOutlined />} title="资源">
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
+          <Menu.Item key="4" icon={<ContainerOutlined />}>
+            资源
+          </Menu.Item>
+          <Menu.Item key="5" icon={<ContainerOutlined />}>
+            国内镜像
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<MailOutlined />} title="更多">
+            <Menu.Item key="6">Option 5</Menu.Item>
+            <Menu.Item key="7">Option 6</Menu.Item>
+            <Menu.Item key="8">Option 7</Menu.Item>
+            <Menu.Item key="9">Option 8</Menu.Item>
           </SubMenu>
         </Menu>
+        </Col>
 
+        </Row>
+          <div id="biglog" align="center">
+            <img  alt="antd_svg" src={antd_svg}></img>
+          </div>
+        <Row>
 
+          
+        </Row>
+        
     </div>
   );
 }
