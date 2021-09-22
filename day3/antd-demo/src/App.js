840acdf8a6bb0a333b6a8fad28728ba96817a05f
vansin <vansin@aliyun.com>
2021-09-22 10:50:38 +0800

    feat: init antd index

---
+++ day3/antd-demo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import { DatePicker } from 'antd';
+import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Ant Design</h1>
+        <DatePicker />
+    </div>
+  );
+}
+
+export default App;
